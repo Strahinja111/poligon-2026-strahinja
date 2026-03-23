301c63e71438cc4af46a059fa315c8a00fc8cc5e
ucenik <ucenik@DESKTOP-M7J8SQ9>
2026-03-23 19:04:27 +0100

    v1

--- poligon.cs
+++ poligon.cs
@@ -17,7 +17,18 @@ namespace poligon_2026_strahinja
         }
         public static poligon unos()
         {
-            return null;
+            Console.WriteLine("koliko temena");
+            int n=Convert.ToInt32(Console.ReadLine());
+            poligon novi = new poligon(n);
+            for (int i = 0; i < n; i++)
+            {
+                novi.teme[i] = new tacka();
+                Console.WriteLine("x koord tacke {0}=", i + 1);
+                novi.teme[i].x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("y koord tacke {0}=", i + 1);
+                novi.teme[i].y = Convert.ToDouble(Console.ReadLine());
+            }
+            return void;
         }
         public void stampa()
         {
